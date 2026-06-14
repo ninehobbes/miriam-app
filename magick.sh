fa7d8e1580b3d90b39f07bf7046426d4f2c22d72
9beach <9beach@gmail.com>
2026-06-14 18:49:28 +0900

    Update misc.

---
+++ magick.sh
@@ -0,0 +1,6 @@
+cd screenshots/
+magick hero-summary.jpg -resize 1242x2688 ~/Downloads/a.jpg
+magick awards-belt.jpg -resize 1242x2688 ~/Downloads/b.jpg
+magick basics-hr-zones.jpg -resize 1242x2688 ~/Downloads/c.jpg
+magick stats-distance.jpg -resize 1242x2688 ~/Downloads/d.jpg
+magick stats-period.jpg -resize 1242x2688 ~/Downloads/e.jpg
